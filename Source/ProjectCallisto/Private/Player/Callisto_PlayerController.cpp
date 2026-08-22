8a8d1cdadc61789d4070cdaa22f976610607ccb4
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-08-23 02:52:27 +0900

    PlayerControlC++_AbilityActionsBP_DONE

---
+++ Source/ProjectCallisto/Private/Player/Callisto_PlayerController.cpp
@@ -0,0 +1,76 @@
+﻿// Copyright Zel Suarez
+
+
+#include "Player/Callisto_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "Engine/LocalPlayer.h"
+#include "GameFramework/Character.h"
+
+void ACallisto_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (!IsValid(InputSubsystem)) return;
+	
+	for (UInputMappingContext* Context:InputMappingContexts)
+	{
+		InputSubsystem->AddMappingContext(Context, 0);
+	}
+	
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (!IsValid(EnhancedInputComponent)) return;
+	
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Jump);
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ThisClass::StopJumping);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
+	
+	EnhancedInputComponent->BindAction(PrimaryAction, ETriggerEvent::Started, this, &ThisClass::Primary);
+	
+}
+
+void ACallisto_PlayerController::Jump()
+{
+	if (!IsValid(GetCharacter())) return;
+	
+	GetCharacter()->Jump();
+}
+
+void ACallisto_PlayerController::StopJumping()
+{
+	if (!IsValid(GetCharacter())) return;
+	
+	GetCharacter()->StopJumping();
+}
+
+void ACallisto_PlayerController::Move(const FInputActionValue& Value)
+{
+	if (!IsValid(GetPawn())) return;
+	
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+	
+	// Find which way is forward
+	const FRotator YawRotation(0.f, GetControlRotation().Yaw, 0.f);
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	
+	GetPawn()->AddMovementInput(ForwardDirection, MovementVector.Y);
+	GetPawn()->AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void ACallisto_PlayerController::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+	
+	AddYawInput(LookAxisVector.X); 
+	AddPitchInput(LookAxisVector.Y);
+}
+
+void ACallisto_PlayerController::Primary()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Primary"));
+}
---
+++ Source/ProjectCallisto/Public/Player/Callisto_PlayerController.h
@@ -0,0 +1,42 @@
+﻿// Copyright Zel Suarez
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Callisto_PlayerController.generated.h"
+
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class PROJECTCALLISTO_API ACallisto_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+protected:
+	virtual void SetupInputComponent() override;
+	
+private:
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Callisto|Input")
+	TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Callisto|Input|Movement")
+	TObjectPtr<UInputAction> JumpAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Callisto|Input|Movement")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Callisto|Input|Movement")
+	TObjectPtr<UInputAction> LookAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Callisto|Input|Abilities")
+	TObjectPtr<UInputAction> PrimaryAction;
+	
+	void Jump();
+	void StopJumping();
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void Primary();
+};
