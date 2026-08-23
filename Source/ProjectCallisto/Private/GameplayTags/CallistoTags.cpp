21389523db53ee0ad45bd956e4a6d7c1c312bbf7
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-08-23 13:47:32 +0900

    NativeGameplayTags_C++_DONE
    Created Gameplay Tags for the project for Primary ability activation. No issues so far.

---
+++ Source/ProjectCallisto/Private/GameplayTags/CallistoTags.cpp
@@ -0,0 +1,9 @@
+﻿#include "GameplayTags/CallistoTags.h"
+
+namespace CallistoTags
+{
+	namespace CallistoAbilities
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(Primary, "CallistoTags.CallistoAbilities.Primary", "Tag for the Primary Ability")
+	}
+}
