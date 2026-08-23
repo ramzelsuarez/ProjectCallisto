afb2d7ea769a4e04b64b452861275ee6c8923f33
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-08-23 16:53:08 +0900

    EnemyClassCreation_C++_DONE
    Create little minions as enemies for Callisto. Added basic AnimBP for Idle animation - just to prove it's working at the moment. No issues.

---
+++ Source/ProjectCallisto/Private/Characters/Callisto_EnemyCharacter.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright Zel Suarez
+
+
+#include "Characters/Callisto_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+ACallisto_EnemyCharacter::ACallisto_EnemyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+UAbilitySystemComponent* ACallisto_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void ACallisto_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (!IsValid(GetAbilitySystemComponent())) return;
+	
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this, this);
+	
+	if (!HasAuthority()) return;
+	
+	GiveStartupAbilities();
+}
+
---
+++ Source/ProjectCallisto/Public/Characters/Callisto_EnemyCharacter.h
@@ -0,0 +1,27 @@
+﻿// Copyright Zel Suarez
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Callisto_BaseCharacter.h"
+#include "Callisto_EnemyCharacter.generated.h"
+
+class UAbilitySystemComponent;
+
+UCLASS()
+class PROJECTCALLISTO_API ACallisto_EnemyCharacter : public ACallisto_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACallisto_EnemyCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	virtual void BeginPlay() override;
+	
+private:
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
