35c206cd159cf42e7fc827e3b3c405ae263239e4
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-08-26 23:02:30 +0900

    AttributeChangeAsyncTaskNodeCreation_C++_DONE

---
+++ Source/ProjectCallisto/Private/Tasks/Callisto_AttributeChangeTask.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright Zel Suarez
+
+
+#include "Tasks/Callisto_AttributeChangeTask.h"
+#include "AbilitySystemComponent.h"
+
+UCallisto_AttributeChangeTask* UCallisto_AttributeChangeTask::ListenForAttributeChange(UAbilitySystemComponent* AbilitySystemComponent, FGameplayAttribute Attribute)
+{
+	UCallisto_AttributeChangeTask* WaitForAttributeChangeTask = NewObject<UCallisto_AttributeChangeTask>();
+	WaitForAttributeChangeTask->ASC = AbilitySystemComponent;
+	WaitForAttributeChangeTask->AttributeToListenFor = Attribute;
+	
+	if (!IsValid(AbilitySystemComponent))
+	{
+		WaitForAttributeChangeTask->RemoveFromRoot();
+		return nullptr;
+	}
+	
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Attribute).AddUObject(WaitForAttributeChangeTask, &UCallisto_AttributeChangeTask::AttributeChanged);
+	
+	return WaitForAttributeChangeTask;
+}
+
+void UCallisto_AttributeChangeTask::EndTask()
+{
+	if (ASC.IsValid())
+	{
+		ASC->GetGameplayAttributeValueChangeDelegate(AttributeToListenFor).RemoveAll(this);
+	}
+	
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UCallisto_AttributeChangeTask::AttributeChanged(const FOnAttributeChangeData& Data)
+{
+	OnAttributeChanged.Broadcast(Data.Attribute, Data.NewValue, Data.OldValue);
+}
