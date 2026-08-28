c219e5cfc2dc36ab6dcf385e5779bc2708ad9140
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-08-29 00:53:17 +0900

    ProjectileClass_C++BP_DONE
    Created projectile class for ranged enemy use.

---
+++ Source/ProjectCallisto/Private/GameObjects/Callisto_Projectile.cpp
@@ -0,0 +1,41 @@
+﻿// Copyright Zel Suarez
+
+
+#include "GameObjects/Callisto_Projectile.h"
+
+#include "AbilitySystemComponent.h"
+#include "Characters/Callisto_PlayerCharacter.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+ACallisto_Projectile::ACallisto_Projectile()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovement");
+	
+	bReplicates = true;
+}
+
+void ACallisto_Projectile::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	
+	ACallisto_PlayerCharacter* PlayerCharacter = Cast<ACallisto_PlayerCharacter>(OtherActor);
+	if (!IsValid(PlayerCharacter)) return;
+	if (!PlayerCharacter->IsAlive()) return;
+	UAbilitySystemComponent* AbilitySystemComponent = PlayerCharacter->GetAbilitySystemComponent();
+	if (!IsValid(AbilitySystemComponent) || !HasAuthority()) return;
+	
+	FGameplayEffectContextHandle ContextHandle = AbilitySystemComponent->MakeEffectContext();
+	FGameplayEffectSpecHandle SpecHandle = AbilitySystemComponent->MakeOutgoingSpec(DamageEffect, 1.f, ContextHandle);
+	
+	// TODO: Use the Damage variable for the amount of damage to cause
+	
+	AbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+	
+	SpawnImpactEffects();
+	Destroy();
+}
+
+
---
+++ Source/ProjectCallisto/Public/GameObjects/Callisto_Projectile.h
@@ -0,0 +1,34 @@
+﻿// Copyright Zel Suarez
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Callisto_Projectile.generated.h"
+
+class UGameplayEffect;
+class UProjectileMovementComponent;
+
+UCLASS()
+class PROJECTCALLISTO_API ACallisto_Projectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ACallisto_Projectile();
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Callisto|Damage", meta = (ExposeOnSpawn, ClampMin = "0.0"))
+	float Damage{10.f};
+	
+	UFUNCTION(BlueprintImplementableEvent, Category = "Callisto|Projectile")
+	void SpawnImpactEffects();
+	
+private:
+	
+	UPROPERTY(VisibleAnywhere, Category = "Callisto|Projectile")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Callisto|Damage")
+	TSubclassOf<UGameplayEffect> DamageEffect;
+};
