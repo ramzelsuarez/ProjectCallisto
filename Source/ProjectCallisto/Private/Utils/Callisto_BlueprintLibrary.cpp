3dc6ae293abda79b782c052b76f06f3e48ca7972
ramzelsuarez <ramzelsuarez@yahoo.com>
2026-08-30 14:51:28 +0900

    Knockback_C++_DONE
    Applied knockback when AOE (secondary skill) is activated. No issues so far and will fix enemy state when it's airborne.

--- Source/ProjectCallisto/Private/Utils/Callisto_BlueprintLibrary.cpp
+++ Source/ProjectCallisto/Private/Utils/Callisto_BlueprintLibrary.cpp
@@ -157,3 +157,48 @@ void UCallisto_BlueprintLibrary::DrawHitBoxOverlapDebugs(const UObject* WorldCon
 		}
 	}
 }
+
+TArray<AActor*> UCallisto_BlueprintLibrary::ApplyKnockback(AActor* AvatarActor, const TArray<AActor*>& HitActors,
+	float InnerRadius, float OuterRadius, float LaunchForceMagnitude, float RotationAngle, bool bDrawDebugs)
+{
+	for (AActor* HitActor : HitActors)
+	{
+		ACharacter* HitCharacter = Cast<ACharacter>(HitActor);
+		if (!IsValid(HitCharacter) || !IsValid(AvatarActor)) return TArray<AActor*>();
+		
+		const FVector HitCharacterLocation = HitCharacter->GetActorLocation();
+		const FVector AvatarLocation = AvatarActor->GetActorLocation();
+		
+		const FVector ToHitActor = HitCharacterLocation - AvatarLocation;
+		const float Distance = FVector::Dist(AvatarLocation, HitCharacterLocation);
+		
+		float LaunchForce = 0.f;
+		if (Distance > OuterRadius) continue;
+		if (Distance <= InnerRadius)
+		{
+			LaunchForce = LaunchForceMagnitude;
+		}
+		else
+		{
+			const FVector2D FalloffRange(InnerRadius, OuterRadius); // input range
+			const FVector2D LaunchForceRange(LaunchForceMagnitude, 0.f); // output range
+			LaunchForce = FMath::GetMappedRangeValueClamped(FalloffRange, LaunchForceRange, Distance);
+		}
+		if (bDrawDebugs) GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Red, FString::Printf(TEXT("LaunchForce: %.2f"), LaunchForce));
+		
+		FVector KnockbackForce = ToHitActor.GetSafeNormal();
+		KnockbackForce.Z = 0.f;
+		
+		const FVector Right = KnockbackForce.RotateAngleAxis(90.f, FVector::UpVector);
+		KnockbackForce = KnockbackForce.RotateAngleAxis(-RotationAngle, Right) * LaunchForce;
+		
+		if (bDrawDebugs)
+		{
+			UWorld* World = GEngine->GetWorldFromContextObject(AvatarActor, EGetWorldErrorMode::LogAndReturnNull);
+			DrawDebugDirectionalArrow(World, HitCharacterLocation, HitCharacterLocation + KnockbackForce, 100.f, FColor::Green, false, 3.f);
+		}
+		
+		HitCharacter->LaunchCharacter(KnockbackForce, true, true);
+	}
+	return HitActors;
+}
--- Source/ProjectCallisto/Public/Utils/Callisto_BlueprintLibrary.h
+++ Source/ProjectCallisto/Public/Utils/Callisto_BlueprintLibrary.h
@@ -49,4 +49,7 @@ public:
 	static TArray<AActor*> HitBoxOverlapTest(AActor* AvatarActor, float HitBoxRadius, float HitBoxForwardOffset = 0.f, float HitBoxElevationOffset = 0.f, bool bDrawDebugs = false);
 	
 	static void DrawHitBoxOverlapDebugs(const UObject* WorldContextObject, const TArray<FOverlapResult>& OverlapResults, const FVector& HitBoxLocation, float HitBoxRadius);
+	
+	UFUNCTION(BlueprintCallable, Category = "Callisto|Abilities")
+	static TArray<AActor*> ApplyKnockback(AActor* AvatarActor, const TArray<AActor*>& HitActors, float InnerRadius, float OuterRadius, float LaunchForceMagnitude, float RotationAngle = 45.f, bool bDrawDebugs = false);
 };
